a6e6caf34769c638456e48ba482789fff0f92e33
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-09 19:57:32 -0300

    feat(repositories): add CategoryRepository, and add dto projection method to countPost

---
+++ src/main/java/tech/devictor/devictor/repositories/CategoryRepository.java
@@ -0,0 +1,17 @@
+package tech.devictor.devictor.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import tech.devictor.devictor.domain.dtos.CategoryResponseDto;
+import tech.devictor.devictor.domain.entities.Category;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("SELECT new tech.devictor.devictor.domain.dtos.CategoryResponseDto(c.id, c.name, COUNT(p.id)) " +
+            "FROM Category c LEFT JOIN c.posts p GROUP BY c.id, c.name")
+    List<CategoryResponseDto> findAllCategoriesWithPostCount();
+}
