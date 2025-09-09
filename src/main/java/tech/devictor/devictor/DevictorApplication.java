eeb5f146aad6d756ec6a230eea6c3237325e843b
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-09 18:52:15 -0300

    initial config

---
+++ src/main/java/tech/devictor/devictor/DevictorApplication.java
@@ -0,0 +1,13 @@
+package tech.devictor.devictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevictorApplication.class, args);
+	}
+
+}
