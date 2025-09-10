e133633937a0c1c7850f2c3da8d2f6960ea1dce2
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-09 22:23:08 -0300

    feat(repositories): add findAllTagsWithPostCount using dto projection

--- src/main/java/tech/devictor/devictor/repositories/TagRepository.java
+++ src/main/java/tech/devictor/devictor/repositories/TagRepository.java
@@ -1,9 +1,17 @@
 package tech.devictor.devictor.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import tech.devictor.devictor.domain.dtos.TagResponseDto;
 import tech.devictor.devictor.domain.entities.Tag;
+
+import java.util.List;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    @Query("SELECT new tech.devictor.devictor.domain.dtos.TagResponseDto(t.id, t.name, COUNT(p.id)) " +
+            "FROM Tag t LEFT JOIN t.posts p GROUP BY t.id, t.name")
+    List<TagResponseDto> findAllTagsWithPostCount();
 }
