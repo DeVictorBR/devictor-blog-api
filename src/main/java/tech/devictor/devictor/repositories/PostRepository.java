004bbbc31d2c9df611514b1601ade6eec1b84a9f
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-12 12:34:10 -0300

    feat(repositories): add repository methods for filtering posts

--- src/main/java/tech/devictor/devictor/repositories/PostRepository.java
+++ src/main/java/tech/devictor/devictor/repositories/PostRepository.java
@@ -3,8 +3,14 @@ package tech.devictor.devictor.repositories;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import tech.devictor.devictor.domain.PostStatus;
+import tech.devictor.devictor.domain.entities.Category;
 import tech.devictor.devictor.domain.entities.Post;
+import tech.devictor.devictor.domain.entities.Tag;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Slice<Post> findBy(Pageable pageable);
+    Slice<Post> findAllByStatusAndCategoryAndTagsContaining(PostStatus status, Category category, Tag tag, Pageable pageable);
+    Slice<Post> findAllByStatusAndCategory(PostStatus status, Category category, Pageable pageable);
+    Slice<Post> findAllByStatusAndTagsContaining(PostStatus status, Tag tag, Pageable pageable);
+    Slice<Post> findAllByStatus(PostStatus status, Pageable pageable);
 }
