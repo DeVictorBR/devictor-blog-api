ea225e69baa2ae0a0e69d85b3526ff310d0fab5c
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-11 22:44:04 -0300

    feat(services): implement create tags method

--- src/main/java/tech/devictor/devictor/services/TagService.java
+++ src/main/java/tech/devictor/devictor/services/TagService.java
@@ -1,9 +1,12 @@
 package tech.devictor.devictor.services;
 
 import tech.devictor.devictor.domain.dtos.TagResponseDto;
+import tech.devictor.devictor.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
     List<TagResponseDto> listTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
--- src/main/java/tech/devictor/devictor/services/impl/TagServiceImpl.java
+++ src/main/java/tech/devictor/devictor/services/impl/TagServiceImpl.java
@@ -2,11 +2,17 @@ package tech.devictor.devictor.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tech.devictor.devictor.domain.dtos.TagResponseDto;
+import tech.devictor.devictor.domain.entities.Tag;
 import tech.devictor.devictor.repositories.TagRepository;
 import tech.devictor.devictor.services.TagService;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +23,28 @@ public class TagServiceImpl implements TagService {
     @Override
     public List<TagResponseDto> listTags() {
         return tagRepository.findAllTagsWithPostCount();
+    }
+
+    @Transactional
+    @Override
+    public List<Tag> createTags(Set<String> tagNames) {
+        List<Tag> existingTags = tagRepository.findByNameIn(tagNames);
+        Set<String> existingTagNames = existingTags.stream()
+                .map(Tag::getName)
+                .collect(Collectors.toSet());
+
+        List<Tag> newTags = tagNames.stream().filter(name -> !existingTagNames.contains(name))
+                .map(name -> Tag.builder()
+                        .name(name)
+                        .posts(new HashSet<>())
+                        .build()).toList();
+
+        List<Tag> savedTags = new ArrayList<>();
+        if (!newTags.isEmpty()) {
+            savedTags = tagRepository.saveAll(newTags);
+        }
+
+        savedTags.addAll(existingTags);
+        return savedTags;
     }
 }
