ae0da2f7557241e89b3147ab634be49c78390d52
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-11 22:42:35 -0300

    feat(repositories): add findByNameIn method

--- src/main/java/tech/devictor/devictor/repositories/TagRepository.java
+++ src/main/java/tech/devictor/devictor/repositories/TagRepository.java
@@ -7,6 +7,7 @@ import tech.devictor.devictor.domain.dtos.TagResponseDto;
 import tech.devictor.devictor.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
@@ -14,4 +15,6 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
     @Query("SELECT new tech.devictor.devictor.domain.dtos.TagResponseDto(t.id, t.name, COUNT(p.id)) " +
             "FROM Tag t LEFT JOIN t.posts p GROUP BY t.id, t.name")
     List<TagResponseDto> findAllTagsWithPostCount();
+
+    List<Tag> findByNameIn(Set<String> names);
 }
