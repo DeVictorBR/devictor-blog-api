056eba30aef7f6f6256f1dfed2916a69601097d0
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-12 12:38:30 -0300

    feat(services): add getCategoryById method to service

--- src/main/java/tech/devictor/devictor/services/CategoryService.java
+++ src/main/java/tech/devictor/devictor/services/CategoryService.java
@@ -2,6 +2,7 @@ package tech.devictor.devictor.services;
 
 import tech.devictor.devictor.domain.dtos.CategoryResponseDto;
 import tech.devictor.devictor.domain.dtos.CreateCategoryDto;
+import tech.devictor.devictor.domain.entities.Category;
 
 import java.util.List;
 
@@ -9,4 +10,5 @@ public interface CategoryService {
     List<CategoryResponseDto> listCategories();
     Long createCategory(CreateCategoryDto createCategoryDto);
     void deleteCategory(Long id);
+    Category getCategoryById(Long id);
 }
