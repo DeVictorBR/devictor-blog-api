8dfcf04f21dd9635f1954e3b68dfaebded3975e1
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-13 11:37:57 -0300

    feat(services): add method to update a post

--- src/main/java/tech/devictor/devictor/services/PostService.java
+++ src/main/java/tech/devictor/devictor/services/PostService.java
@@ -4,9 +4,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import tech.devictor.devictor.domain.dtos.CreatePostRequestDto;
 import tech.devictor.devictor.domain.dtos.PostResponseDto;
+import tech.devictor.devictor.domain.dtos.UpdatePostRequestDto;
 
 public interface PostService {
     Slice<PostResponseDto> getAllPosts(Long categoryId, Long tagId, Pageable pageable);
     Slice<PostResponseDto> getAllDrafts(Pageable pageable);
     PostResponseDto createPost(CreatePostRequestDto dto);
+    PostResponseDto updatePost(Long id , UpdatePostRequestDto dto);
 }
--- src/main/java/tech/devictor/devictor/services/impl/PostServiceImpl.java
+++ src/main/java/tech/devictor/devictor/services/impl/PostServiceImpl.java
@@ -8,14 +8,17 @@ import org.springframework.transaction.annotation.Transactional;
 import tech.devictor.devictor.domain.PostStatus;
 import tech.devictor.devictor.domain.dtos.CreatePostRequestDto;
 import tech.devictor.devictor.domain.dtos.PostResponseDto;
+import tech.devictor.devictor.domain.dtos.UpdatePostRequestDto;
 import tech.devictor.devictor.domain.entities.Category;
 import tech.devictor.devictor.domain.entities.Post;
 import tech.devictor.devictor.domain.entities.Tag;
+import tech.devictor.devictor.exceptions.PostNotFoundException;
 import tech.devictor.devictor.repositories.PostRepository;
 import tech.devictor.devictor.services.CategoryService;
 import tech.devictor.devictor.services.PostService;
 import tech.devictor.devictor.services.TagService;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
@@ -90,5 +93,41 @@ public class PostServiceImpl implements PostService {
         Post savedPost = postRepository.save(post);
 
         return PostResponseDto.toDto(savedPost);
+    }
+
+    @Override
+    @Transactional
+    public PostResponseDto updatePost(Long id, UpdatePostRequestDto dto) {
+        Post post = getPostEntityById(id);
+
+        if (dto.title() != null) {
+            post.setTitle(dto.title());
+        }
+        if (dto.content() != null) {
+            post.setContent(dto.content());
+        }
+        if (dto.status() != null) {
+            post.setStatus(dto.status());
+        }
+
+        Category category = null;
+        if (dto.categoryId() != null) {
+            category = categoryService.getCategoryById(dto.categoryId());
+        }
+        post.setCategory(category);
+
+        Set<Tag> tags = new HashSet<>();
+        if (dto.tagIds() != null) {
+            tags = tagService.getTagsByIds(dto.tagIds());
+        }
+        post.setTags(tags);
+
+        Post savedPost = postRepository.save(post);
+        return PostResponseDto.toDto(savedPost);
+    }
+
+    private Post getPostEntityById(Long id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(String.format("Post not found with id: %d", id)));
     }
 }
