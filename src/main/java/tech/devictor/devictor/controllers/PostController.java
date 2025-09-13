e25f9fe25d324c4f52481dbc4e38759cb7bec0fe
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-13 10:55:53 -0300

    feat(controllers): add API endpoint to create post

--- src/main/java/tech/devictor/devictor/controllers/PostController.java
+++ src/main/java/tech/devictor/devictor/controllers/PostController.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import tech.devictor.devictor.domain.dtos.CreatePostRequestDto;
 import tech.devictor.devictor.domain.dtos.PostResponseDto;
 import tech.devictor.devictor.services.PostService;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(path = "/api/v1/posts")
@@ -28,6 +32,16 @@ public class PostController {
     public ResponseEntity<Slice<PostResponseDto>> getDrafts(Pageable pageable) {
         Slice<PostResponseDto> drafts = postService.getAllDrafts(pageable);
         return ResponseEntity.ok(drafts);
+    }
 
+    @PostMapping
+    public ResponseEntity<PostResponseDto> createPost(@RequestBody CreatePostRequestDto dto) {
+        PostResponseDto responseDto = postService.createPost(dto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDto.id())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDto);
     }
 }
